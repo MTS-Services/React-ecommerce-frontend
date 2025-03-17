2a9efb71ab41c9b4236df1a385331b6c97d25c72
Nayem Islam <inaeem707@gmail.com>
2025-03-17 19:10:19 +0600

    add products image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import CartView from "./pages/cart/CartView";
+import CheckoutView from "./pages/Chekout/CheckoutView";
 
 function App() {
   return (
-    <div className="flex bg-red-500 pt-0">
-      <h1>Nayem</h1>
-    </div>
+    <section className="px-44">
+      <CartView />
+    </section>
   );
 }
 
