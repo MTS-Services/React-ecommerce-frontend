791beefee78475386b6d852226a17bfd8f55384b
shakilmonsi <107542773+shakilmonsi@users.noreply.github.com>
2025-03-18 08:18:52 +0600

    added heroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
+import HeroSection from "./component/HeroSection/HeroSection";
+import Testing from "./component/HeroSection/testing/testing";
 
 function App() {
   return (
     <>
+      <HeroSection></HeroSection>
       <div className="flex bg-red-500 pt-0">
         <h1>Nayem</h1>
       </div>
