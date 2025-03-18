ed4d30850a96d27240b9333201bb4edd06ddd984
shakilmonsi <107542773+shakilmonsi@users.noreply.github.com>
2025-03-18 09:11:45 +0600

    add package solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import "./App.css";
-<<<<<<< HEAD
 import HeroSection from "./component/HeroSection/HeroSection";
-import Testing from "./component/HeroSection/testing/testing";
-
-function App() {
-  return (
-    <>
-      <HeroSection></HeroSection>
-      <div className="flex bg-red-500 pt-0">
-        <h1>Nayem</h1>
-      </div>
-    </>
-=======
 import CartView from "./pages/cart/CartView";
 import CheckoutView from "./pages/Chekout/CheckoutView";
 
 function App() {
   return (
     <section className="px-44">
+      <HeroSection></HeroSection>
+
       <CartView />
     </section>
->>>>>>> 2a9efb71ab41c9b4236df1a385331b6c97d25c72
   );
 }
 
