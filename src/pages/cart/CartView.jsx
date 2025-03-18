9c188b12d7a74017bf5218d6b87faf65a8a647b6
Nayem Islam <inaeem707@gmail.com>
2025-03-18 08:55:05 +0600

    fix: changes npm package

--- src/pages/cart/CartView.jsx
+++ src/pages/cart/CartView.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
 const CartView = () => {
   return (
@@ -6,7 +7,7 @@ const CartView = () => {
       <h1 className="mb-6 text-3xl font-semibold">Cart</h1>
       <div className="flex gap-6">
         {/* Left Column: Summary */}
-        <div className="w-1/3 rounded-lg border border-gray-300 bg-[#f8f8fa] p-4 shadow-md">
+        <div className="w-1/3 rounded-lg border border-gray-300 bg-[#f8f8fa] p-6">
           <div>
             <h2 className="mb-4 text-xl font-semibold">Summary</h2>
             <h2 className="mb-4 text-xl font-semibold">Estimate Shipping</h2>
@@ -69,44 +70,53 @@ const CartView = () => {
         </div>
 
         {/* Right Column: Product Item Table */}
-        <div className="w-2/3 rounded-lg border border-gray-300 p-4 shadow-md">
+        <div className="w-2/3 space-y-2 rounded-lg p-4">
           <h2 className="mb-4 text-xl font-semibold">Your Cart Items</h2>
           <table className="w-full table-auto">
-            <thead>
+            <thead className="border-b border-b-gray-200">
               <tr>
                 <th className="px-4 py-2 text-left">Product</th>
-                <th className="px-4 py-2 text-left">Price</th>
-                <th className="px-4 py-2 text-left">Quantity</th>
-                <th className="px-4 py-2 text-left">Total</th>
-                <th className="px-4 py-2 text-left">Actions</th>
+                <th className="px-4 py-2 text-center">Price</th>
+                <th className="px-4 py-2 text-center">Quantity</th>
+                <th className="px-4 py-2 text-center">Total</th>
+                <th className="px-4 py-2 text-right">Actions</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="px-4 py-2">Product 1</td>
-                <td className="px-4 py-2">$25.99</td>
-                <td className="px-4 py-2">2</td>
-                <td className="px-4 py-2">$51.98</td>
-                <td className="px-4 py-2">
-                  <button className="rounded-md bg-red-500 px-4 py-2 text-white">
-                    Remove
+              <tr className="border-b border-b-gray-200">
+                <td className="px-4 py-2 text-left">Product 1</td>
+                <td className="px-4 py-2 text-center">$25.99</td>
+                <td className="px-4 py-2 text-center">2</td>
+                <td className="px-4 py-2 text-center">$51.98</td>
+                <td className="px-4 py-2 text-right">
+                  <button className="cursor-pointer">
+                    <FaTrash className="h-5 w-5" />
                   </button>
                 </td>
               </tr>
 
-              <tr>
-                <td className="px-4 py-2">Product 2</td>
-                <td className="px-4 py-2">$40.00</td>
-                <td className="px-4 py-2">1</td>
-                <td className="px-4 py-2">$40.00</td>
-                <td className="px-4 py-2">
-                  <button className="rounded-md bg-red-500 px-4 py-2 text-white">
-                    Remove
+              <tr className="border-b border-b-gray-200">
+                <td className="px-4 py-2 text-left">Product 2</td>
+                <td className="px-4 py-2 text-center">$40.00</td>
+                <td className="px-4 py-2 text-center">1</td>
+                <td className="px-4 py-2 text-center">$40.00</td>
+                <td className="px-4 py-2 text-right">
+                  <button className="cursor-pointer">
+                    <FaTrash className="h-5 w-5" />
                   </button>
                 </td>
               </tr>
             </tbody>
           </table>
+
+          <div className="flex justify-between">
+            <a href="#" className="cursor-pointer text-xl font-bold underline">
+              Continue Shopping
+            </a>
+            <button className="cursor-pointer rounded-sm bg-blue-800 px-4 py-2 text-white">
+              Checkout
+            </button>
+          </div>
         </div>
       </div>
     </section>
