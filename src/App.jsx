ca0536645d4f66f28df99004c58696e6233cb36c
Nayem Islam <inaeem707@gmail.com>
2025-03-23 15:45:45 +0600

    (fix) setup router

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
 import { Outlet } from "react-router-dom";
-import HeroSection from "./components/Testing";
-import ProductCard2 from "./components/ProductCard2";
 
 function App() {
   return (
     <section className="">
       <Navbar />
-      <HeroSection />
-      <ProductCard2 />
       <Outlet />
       <Footer />
     </section>
