d4af05d41ec94e29b85bb10b2c0017bb237adec5
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-04-17 12:24:23 +0600

    work

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,15 @@ function App() {
   return (
     <section>
       <Navbar title="kamrul" />
+      <Home />
       <Outlet />
+      <ProductCard />
+      <SingleProduct />
+      <Dashboard />
       <Footer />
     </section>
   );
 }
 export default App;
 // In the App component, we have imported the AdminDashboard component and used it in the JSX. We have also updated the import statement to PascalCase to match the component naming convention.
+import Home from "./pages/home/Home";
