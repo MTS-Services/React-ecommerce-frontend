2797a93da6512f67b29551262e3891cba085e31e
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-04-19 14:20:12 +0600

    work on about page

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,48 +1,42 @@
 import React, { useState, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { GoMail } from "react-icons/go";
-import { HiOutlinePhone } from "react-icons/hi";
-import { SlSocialFacebook } from "react-icons/sl";
-import { TfiTwitter } from "react-icons/tfi";
-import { RxInstagramLogo } from "react-icons/rx";
-import { BsSearch } from "react-icons/bs";
-
-const SearchBar = ({ searchOpen, setSearchOpen }) => {
-  return (
-    <div className="relative">
-      <button
-        onClick={() => setSearchOpen(!searchOpen)}
-        className="p-2 text-gray-600 transition-colors hover:text-[#19B2E7]"
-      >
-        <BsSearch size={25} />
-      </button>
-
-      <AnimatePresence>
-        {searchOpen && (
-          <motion.div
-            initial={{ opacity: 0, width: 0 }}
-            animate={{ opacity: 1, width: "300px" }}
-            exit={{ opacity: 0, width: 0 }}
-            className="absolute top-full right-0 mt-2"
-          >
-            <input
-              type="text"
-              placeholder="Search products..."
-              className="w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-transparent focus:ring-2 focus:ring-[#19B2E7] focus:outline-none"
-              autoFocus
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
-  );
-};
+import { motion, AnimatePresence } from "framer-motion";
+import { GoMail, GoSearch } from "react-icons/go";
+import {
+  SlSocialFacebook,
+  SlSocialLinkedin,
+  SlCallEnd,
+  SlSocialTwitter,
+  SlSocialInstagram,
+} from "react-icons/sl";
+import { BsCart3 } from "react-icons/bs";
+import { AiOutlineDelete } from "react-icons/ai";
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: "Product 1", price: "$10", image: "/img/products/1_1.jpg" },
+    { id: 2, name: "Product 2", price: "$20", image: "/img/products/1_1.jpg" },
+  ]);
 
   const links = ["Home", "Products", "Pages", "Contact", "About"];
+
+  const toggleCart = () => setIsCartOpen(!isCartOpen);
+
+  const addToCart = () => {
+    const newItem = {
+      id: cartItems.length + 1,
+      name: `Product ${cartItems.length + 1}`,
+      price: "$30",
+      image: "/img/products/1_1.jpg",
+    };
+    setCartItems([...cartItems, newItem]);
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -70,29 +64,39 @@ const Header = () => {
             </span>
             <span className="flex items-center text-gray-600">
               <span className="mr-1">
-                <HiOutlinePhone size={20} />
+                <SlCallEnd size={20} />
               </span>
               +880 1886-159495
             </span>
           </div>
           <div className="flex items-center space-x-3">
             <a
-              href="#"
+              href="https://www.facebook.com/MakTechSolution/"
+              target="_blank"
               className="text-gray-600 transition-colors hover:text-[#19B2E7]"
             >
               <SlSocialFacebook size={20} />
             </a>
             <a
-              href="#"
+              href="https://x.com/MakTechSolution"
+              target="_blank"
               className="text-gray-600 transition-colors hover:text-[#19B2E7]"
             >
-              <TfiTwitter size={20} />
+              <SlSocialTwitter size={20} />
             </a>
             <a
-              href="#"
+              href="https://www.instagram.com/maktechsolution/"
+              target="_blank"
               className="text-gray-600 transition-colors hover:text-[#19B2E7]"
             >
-              <RxInstagramLogo size={20} />
+              <SlSocialInstagram size={20} />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/maktechsolution"
+              target="_blank"
+              className="text-gray-600 transition-colors hover:text-[#19B2E7]"
+            >
+              <SlSocialLinkedin size={20} />
             </a>
           </div>
         </div>
@@ -130,8 +134,131 @@ const Header = () => {
                 );
               })}
             </ul>
-            {/* Search Icon */}
-            <SearchBar searchOpen={searchOpen} setSearchOpen={setSearchOpen} />
+
+            {/* Search */}
+            <div className="relative">
+              <button
+                onClick={() => setSearchOpen(!searchOpen)}
+                className="p-2 text-gray-600 transition-colors hover:text-[#19B2E7]"
+              >
+                <GoSearch size={25} />
+              </button>
+              <AnimatePresence>
+                {searchOpen && (
+                  <motion.div
+                    initial={{ opacity: 0, width: 0 }}
+                    animate={{ opacity: 1, width: "300px" }}
+                    exit={{ opacity: 0, width: 0 }}
+                    className="absolute top-full right-0 mt-2 rounded-lg bg-white shadow-lg"
+                  >
+                    <input
+                      type="text"
+                      placeholder="Search products..."
+                      className="w-full rounded-md border border-gray-300 p-2 shadow-sm focus:ring-2 focus:ring-[#19B2E7] focus:outline-none"
+                      autoFocus
+                    />
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
+
+            {/* Cart */}
+            <div className="relative">
+              <button
+                onClick={toggleCart}
+                className="relative p-4 text-gray-600 transition-colors hover:text-[#19B2E7]"
+              >
+                <BsCart3 size={25} />
+                <span className="absolute -top-1 -right-1 mt-1 flex h-4 w-4 items-center justify-center rounded-full bg-[#152550] p-3 text-xs text-white">
+                  {cartItems.length}
+                </span>
+              </button>
+              <AnimatePresence>
+                {isCartOpen && (
+                  <motion.div
+                    initial={{ x: "100%" }}
+                    animate={{ x: 0 }}
+                    exit={{ x: "100%" }}
+                    transition={{ type: "spring", stiffness: 300 }}
+                    className="absolute right-0 z-10 mt-4 h-screen w-[350px] rounded-lg bg-white p-4 shadow-lg"
+                  >
+                    <h3 className="mb-2 font-semibold">Your Cart</h3>
+                    {cartItems.length === 0 ? (
+                      <p>Your cart is empty.</p>
+                    ) : (
+                      <div>
+                        {cartItems.map((item) => (
+                          <div key={item.id} className="mb-4 flex items-center">
+                            <div className="flex w-full items-center justify-between rounded-lg border border-gray-300 bg-white p-4 shadow-md transition-all duration-300 hover:shadow-lg">
+                              <div className="flex gap-4">
+                                <img
+                                  src={item.image}
+                                  alt={item.name}
+                                  className="h-16 w-16 rounded-lg object-cover"
+                                />
+                                <div>
+                                  <h4 className="text-xl font-semibold text-gray-900">
+                                    {item.name}
+                                  </h4>
+                                  <p className="text-sm font-medium text-gray-600">
+                                    {item.price}
+                                  </p>
+                                </div>
+                              </div>
+                              <button
+                                onClick={() => removeFromCart(item.id)}
+                                className="rounded-md bg-red-600 p-2 text-sm font-medium text-white transition-all duration-300 hover:bg-red-700"
+                              >
+                                <AiOutlineDelete className="text-[20px]" />
+                              </button>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                    <div className="border-t-2 border-gray-200 py-6">
+                      <div className="flex flex-col space-y-4 px-4">
+                        <div className="flex items-center justify-between">
+                          <p className="text-[18px] font-bold text-gray-500">
+                            Sub-Total:
+                          </p>
+                          <p className="text-[20px] font-bold text-[#545454]">
+                            $2134
+                          </p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <p className="text-[18px] font-bold text-gray-500">
+                            Vat (20%):
+                          </p>
+                          <p className="text-[20px] font-bold text-[#545454]">
+                            $2134
+                          </p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                          <p className="text-[18px] font-bold text-gray-500">
+                            Total:
+                          </p>
+                          <p className="text-[20px] font-bold text-[#545454]">
+                            $2134
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between gap-2 text-center">
+                      <button className="w-full rounded bg-blue-500 py-2 text-[10px] text-white">
+                        Go to Checkout
+                      </button>
+                      <button
+                        onClick={addToCart}
+                        className="w-full rounded bg-green-500 py-2 text-[10px] text-white"
+                      >
+                        Add New Product
+                      </button>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </div>
           </nav>
         </div>
       </div>
--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,7 +1,181 @@
 import React from "react";
+import {
+  FaShippingFast,
+  FaHeadset,
+  FaExchangeAlt,
+  FaLock,
+  FaChevronDown,
+} from "react-icons/fa";
 
 const AboutPage = () => {
-  return <div>About</div>;
+  const services = [
+    {
+      icon: <FaShippingFast className="text-4xl text-[#19B2E7]" />,
+      title: "Free Shipping",
+      desc: "Free shipping on all US order or order above $200",
+    },
+    {
+      icon: <FaHeadset className="text-4xl text-[#19B2E7]" />,
+      title: "24x7 Support",
+      desc: "Contact us 24 hours a day, 7 days a week",
+    },
+    {
+      icon: <FaExchangeAlt className="text-4xl text-[#19B2E7]" />,
+      title: "30 Days Return",
+      desc: "Simply return it within 30 days for an exchange",
+    },
+    {
+      icon: <FaLock className="text-4xl text-[#19B2E7]" />,
+      title: "Payment Secure",
+      desc: "Your payments are secure with industry encryption",
+    },
+  ];
+
+  const faqs = [
+    {
+      question: "What is the multi vendor services?",
+      answer: ["Multi vendor mane ek e-commerce platform e onek seller thake."],
+    },
+    {
+      question: "How to buy many products at a time?",
+      answer: ["Cart e onek product add koro."],
+    },
+    {
+      question: "How to buy many products at a time?",
+      answer: ["lorem ipsum dolor sit amet."],
+    },
+    {
+      question: "How to buy many products at a time?",
+      answer: ["lorem change sentence."],
+    },
+    {
+      question: "How to buy many products at a time?",
+      answer: ["lorem ipsum dolor sit amet. lorem ipsum dolor sit amet."],
+    },
+    {
+      question: "How to buy many products at a time?",
+      answer: [
+        "lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.",
+      ],
+    },
+  ];
+
+  return (
+    <main className="mt-30 bg-white font-sans">
+      {/* Who We Are Section */}
+      <section className="mx-auto max-w-7xl px-6 py-20">
+        <div className="flex flex-col gap-12 lg:flex-row">
+          {/* Image Grid */}
+          <div className="flex max-w-full flex-1 flex-col gap-4 sm:flex-row lg:flex-col xl:flex-row">
+            <div className="flex-1">
+              <img
+                src="/img/banner1.png"
+                alt="Shop"
+                className="h-full w-full rounded-lg object-cover"
+              />
+            </div>
+            <div className="flex flex-1 flex-col gap-4">
+              <img
+                src="/img/banner2.png"
+                alt="Farmer"
+                className="h-full w-full rounded-lg object-cover"
+              />
+              <img
+                src="/img/banner2.png"
+                alt="Greenhouse"
+                className="h-full w-full rounded-lg object-cover"
+              />
+            </div>
+          </div>
+
+          {/* Text Content */}
+          <div className="flex-1 space-y-5">
+            <h2 className="text-3xl font-bold text-gray-800 md:text-4xl">
+              Who We <span className="text-[#19B2E7]">Are?</span>
+            </h2>
+            <p className="text-sm font-semibold text-gray-600 uppercase">
+              We’re here to serve only the best products for you. Enriching your
+              homes with the best essentials.
+            </p>
+            <div className="space-y-4 text-sm leading-relaxed text-gray-500">
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry standard dummy text
+                ever since the 1500s...
+              </p>
+              <p>
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
+              <p>
+                It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum, and more recently with desktop
+                publishing software like Aldus PageMaker.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Our Services Section */}
+      <section className="mx-auto max-w-7xl px-6 text-center sm:py-10 md:py-6 lg:py-20">
+        <h2 className="text-3xl font-bold text-gray-800 md:text-4xl">
+          Our <span className="text-[#19B2E7]">Services</span>
+        </h2>
+        <p className="mx-auto mt-2 max-w-xl text-sm text-gray-600 md:text-base">
+          Customer service should not be a department. It should be the entire
+          company.
+        </p>
+
+        <div className="mt-10 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          {services.map((service, index) => (
+            <div
+              key={index}
+              className="rounded-xl border border-gray-200 p-6 text-center transition-shadow hover:shadow-lg"
+            >
+              <div className="mb-4 flex justify-center">{service.icon}</div>
+              <h4 className="mb-2 text-lg font-semibold text-gray-800">
+                {service.title}
+              </h4>
+              <p className="text-sm text-gray-600">{service.desc}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Frequently Asked Questions */}
+      <section className="mx-auto max-w-7xl px-4 py-20 sm:px-6 lg:px-8">
+        <div className="mb-10 text-center">
+          <h2 className="text-3xl font-bold text-gray-800 md:text-4xl">
+            Frequently Asked <span className="text-[#19B2E7]">Questions</span>
+          </h2>
+          <p className="mt-2 text-sm text-gray-600">
+            Customer service management.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {faqs.map((faq, index) => (
+            <details
+              key={index}
+              className="group rounded-md border border-gray-200 bg-gray-50 p-4"
+            >
+              <summary className="flex cursor-pointer items-center justify-between font-medium text-gray-800">
+                {faq.question}
+                <FaChevronDown className="h-4 w-4 text-[#19B2E7] transition-transform duration-200 group-open:rotate-180" />
+              </summary>
+
+              <div className="mt-2 space-y-2 text-sm text-gray-600">
+                {faq.answer.map((para, i) => (
+                  <p key={i}>{para}</p>
+                ))}
+              </div>
+            </details>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default AboutPage;
