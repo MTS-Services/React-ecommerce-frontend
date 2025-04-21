6e8fabc966546616b091d6152a913562af7da066
Nayem Islam <inaeem707@gmail.com>
2025-04-21 17:11:08 +0600

    (fix) resolve issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { CartProvider } from "./context/cartContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.jsx";
 
